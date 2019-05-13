8734cc54ebb27a4a67947b5517b107e3d21a3782
fujie <1416188864@qq.com>
2019-05-13 19:05:55 +0800

    '期末测试简答题完成联调'

--- src/components/study-course/final-test.vue
+++ src/components/study-course/final-test.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
       <div>
-          <p>一、单选题</p>
+          <p>一、单选题: 总分100分</p>
           <p>最终得分：{{score}}</p>
           <div v-for="(item,idx) in radioData" :key="idx">
               <p>{{idx+1}}、{{item.choiceQuestion}}</p>
@@ -26,7 +26,22 @@
           
       </div>
       <div>
-          <p>二、简答题</p>
+          <p v-if="questionData.length">二、简答题</p>
+          <p>最终得分：{{qustionScore}}</p>
+          <div v-for="(item,index) in questionData" :key="index">
+            <p>{{index+1}}、{{item.questionContent}}</p>
+            <div class="anwser-box">
+                <el-input
+                    type="textarea"
+                    :autosize="{ minRows:10, maxRows: 10}"
+                    placeholder="请输入内容"
+                    v-model="questionAnswer[index]">
+                </el-input>
+                <p class="questionbutton">
+                    <el-button type="primary" :disabled="!questionAnswer[index]" @click="postQuestionAnwser(item.questionId,questionAnswer[index])">提交答案</el-button>
+                </p>
+            </div>
+          </div>
       </div>
   </div>
 </template>
@@ -37,11 +52,14 @@ export default {
     return {
       disabled:false,
       radioData:[],         //单选题列表
-      checkedValue:[],         //选择的答案列表
+      checkedValue:[],         //单选的答案列表
       testData:[],              //复制的 checkedValue
-      answerData:[],            //后台返回的正确答案列表
+      answerData:[],            //后台返回的单选正确答案列表
       yesCount:0,               //答对的题数
-      score:0                   //最后得分
+      score:0,                   //单选最后得分
+      questionData:[],            //简答题列表
+      questionAnswer:[],            //简答题答案
+      qustionScore:0                //简答题分数
     }
   },
   computed:{
@@ -58,6 +76,8 @@ export default {
       let vm = this;
       vm.getradioList();
       vm.getRadioScore();
+      vm.getQuestionList();
+      vm.getQuestionScore();
   },
   methods:{
 
@@ -139,6 +159,9 @@ export default {
            }
        },
 
+        /**
+         * 获取单选成绩
+         */
        getRadioScore(){
            let vm =this;
             vm.$axios.get(`/score/user/course/${vm.userId}/${vm.courseId}`)
@@ -155,7 +178,79 @@ export default {
                     .catch(err => {
                     return false
                     });
-       }
+       },
+
+       /**
+        * 获取简答题列表
+        */
+        getQuestionList(){
+            
+            let vm = this;
+            vm.$axios.post('/question/type/courseOrSectionId',{
+                questionType:2,
+                questionCourseOrSectionId:vm.courseId
+            })
+            .then(function(res){
+            let data =res.data;
+            if(data.result){
+                vm.questionData= data.data;
+            }
+            })
+            .catch(err => {
+                return false
+            });
+        },
+
+        /**
+         * 提交简答题答案
+         */
+
+        postQuestionAnwser(id,anwser){
+            let vm = this;
+            vm.$axios.post('/work/work',{
+                userId:vm.userId,
+                workQuestionId:id,
+	            workContent:anwser
+            })
+            .then(function(res){
+                let data =res.data;
+                if(data.result){
+                    vm.$message({
+                        type: 'success',
+                        message: '提交成功'
+                    });
+                }
+            })
+            .catch(err => {
+                return false
+            });
+        },
+
+        /**
+         * 获取简答题成绩
+         */
+
+        getQuestionScore(){
+            let vm =this;
+            vm.$axios.get(`/work/score/${vm.courseId}/${vm.userId}`)
+                .then(function(res){
+                    let data =res.data;
+                    if(data.result){
+                        vm.questionScore= data.data.finalScore;
+                        // if(vm.score!=0){
+                        //     vm.disabled = true;
+                        // }
+                        vm.$message({
+                        type: 'success',
+                        message: '获取简答题成绩成功'
+                    });
+                        
+                    }
+                    })
+                    .catch(err => {
+                    return false
+                    });
+        }
   }
 }
 </script>
@@ -167,5 +262,13 @@ export default {
 }
 .tijiao{
     text-align: center;
+}
+.anwser-box{
+    width: 60%;
+}
+
+.questionbutton{
+    padding-left: 550px;
+    padding-top: 20px;
 }
 </style>
